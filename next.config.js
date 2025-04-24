6ab6cfa4bc7aa2566d55fec14599736fc5b87572
garsetayusuf <garsetayusuf@gmail.com>
2025-04-25 00:22:48 +0700

    fix: resend package error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ["@react-email/render"],
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
